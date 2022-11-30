657506fa2bb866952466ab6069acfb3f118121a9
Austin Papritz <austin@papritz.dev>
2022-11-30 14:06:04 -0800

    family of families logs family objects with nested fuzzy_bunnies objects

--- families/families.js
+++ families/families.js
@@ -9,9 +9,11 @@ logoutButton.addEventListener('click', () => {
     logout();
 });
 
-function displayFamilies() {
+async function displayFamilies() {
     // fetch families from supabase
+    const families = await getFamilies();
     // clear out the familiesEl
+    familiesEl.innerHTML = '';
     // loop through each family and for each family:
     // create three elements for each family, one for the whole family, one to hold the name, and one to hold the bunnies
     // your HTML Element should look like this:
@@ -30,6 +32,9 @@ function displayFamilies() {
     // append this bunnyEl to the bunniesEl
     // append the bunniesEl and nameEl to the familyEl
     // append the familyEl to the familiesEl
+    for (let family of families) {
+        console.log(family, 'family');
+    }
 }
 
 window.addEventListener('load', async () => {
--- fetch-utils.js
+++ fetch-utils.js
@@ -11,7 +11,7 @@ export function getUser() {
 
 export async function getFamilies() {
     // fetch all families and their bunnies
-    const response = await client.from('loving_families').select('*');
+    const response = await client.from('loving_families').select('*, fuzzy_bunnies(*)');
     return checkError(response);
 }
 
@@ -23,7 +23,6 @@ export async function deleteBunny(id) {
 export async function createBunny(bunny, family) {
     // create a bunny using the bunny argument
     const response = await client.from('fuzzy_bunnies').insert({ name: bunny, family_id: family });
-    console.log(response, 'bunny');
     return checkError(response);
 }
 
